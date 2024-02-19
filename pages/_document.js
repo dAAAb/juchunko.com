620be93638df45c3b04f8dd2f420e05dcd84a98e
gnehs <jayuiop@gmail.com>
2024-02-19 12:33:58 +0800

    Add Google Analytics tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -9,13 +9,13 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://rsms.me/" />
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
           <script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.18/build/spline-viewer.js" />
-          <GoogleAnalytics gaId="G-9ZBHKW4YJF" />
         </Head>
         <body>
           <SkipNavLink styled />
           <Main />
           <NextScript />
         </body>
+        <GoogleAnalytics gaId="G-9ZBHKW4YJF" />
       </Html>
     )
   }
