33a0f3fbea272ae972bbe736d58fb59fd3c94fe9
gnehs <jayuiop@gmail.com>
2024-02-27 15:15:19 +0800

    updated site title

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,11 +3,13 @@ import { useRouter } from 'next/router'
 import Footer from './components/footer'
 const translations = {
   'zh-TW': {
+    logo: '科技立委葛如鈞．寶博士',
     toc: '目錄',
     search_placeholder: '搜尋⋯⋯',
     edit_this_page: '在 GitHub 編輯這個頁面',
   },
   'en-US': {
+    logo: 'JU CHUN KO',
     toc: 'Table of Contents',
     search_placeholder: 'Search...',
     edit_this_page: 'Edit this page on GitHub',
@@ -19,7 +21,7 @@ function localeTranslation(key: string) {
   return translations[locale][key] || translations['en-US'][key]
 }
 const config: DocsThemeConfig = {
-  logo: <span className="nx-font-semibold">JUCHUNKO</span>,
+  logo: () => <span className="nx-font-semibold">{localeTranslation('logo')}</span>,
   project: {
     link: 'https://github.com/dAAAb/juchunko.com',
   },
@@ -28,11 +30,11 @@ const config: DocsThemeConfig = {
     const { asPath } = useRouter()
     if (asPath !== '/') {
       return {
-        titleTemplate: '%s – 科技立委葛如鈞',
+        titleTemplate: '%s – 科技立委葛如鈞．寶博士',
       }
     } else {
       return {
-        title: '先進國會・共識未來 - 科技立委葛如鈞',
+        title: '先進國會・共識未來 - 科技立委葛如鈞．寶博士',
       }
     }
   },
@@ -40,7 +42,7 @@ const config: DocsThemeConfig = {
     <>
       <link rel="icon" type="image/svg+xml" href="/favicon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="先進國會・共識未來 - JUCHUNKO.COM" />
+      <meta property="og:title" content="先進國會・共識未來 - 科技立委葛如鈞．寶博士" />
       <meta
         property="og:description"
         content="歡迎來到葛如鈞（寶博士）的寶博宇宙說明書，在這裡你可以學會如何操作、使用和召喚多元宇宙列車以及公僕寶博。"
