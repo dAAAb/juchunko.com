7b5654681b2fd58d700dab728b6a66f0c8880f72
gnehs <gnehs@gnehs.net>
2024-05-21 17:36:22 +0800

    updated SpeechAI style

--- components/bot/SpeechAI.tsx
+++ components/bot/SpeechAI.tsx
@@ -1,13 +1,13 @@
 'use client'
-import { useEffect, useState, useRef, use } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { motion, AnimatePresence, useMotionValue } from 'framer-motion'
-import { BotMessageSquare, Minus, Bot, User, RotateCcw, MessageCircleQuestion, Send, Copy, Check } from 'lucide-react'
+import { BotMessageSquare, Minus, Bot, User, RotateCcw, ArrowRight, Send, Copy, Check } from 'lucide-react'
 import { twMerge } from 'tailwind-merge'
 import { useCompletion } from 'ai/react'
 import { useLocalStorage } from 'usehooks-ts'
 import Markdown from 'react-markdown'
 import { usePathname } from 'next/navigation'
-function Message({ from, content }: { from: 'me' | 'ai'; content: string }) {
+function Message({ from, content, showCopy = true }: { from: 'me' | 'ai'; content: string; showCopy?: boolean }) {
   const [copied, setCopied] = useState(false)
   async function copyToClipboard(text: string) {
     if ('clipboard' in navigator) {
@@ -20,28 +20,17 @@ function Message({ from, content }: { from: 'me' | 'ai'; content: string }) {
     <motion.div
       initial={{ opacity: 0, x: from === 'me' ? 50 : -50 }}
       animate={{ opacity: 1, x: 0 }}
-      exit={{ opacity: 0, x: from === 'me' ? 50 : -50, height: 0 }}>
+      exit={{ opacity: 0, x: from === 'me' ? 50 : -50 }}>
       <div className={twMerge('mb-2 flex w-full items-start gap-2', from === 'me' ? 'flex-row-reverse' : '')}>
-        {from === 'me' && (
-          <div
-            className={twMerge(
-              'rounded-full p-2',
-              from === 'me'
-                ? 'bg-gray-100 text-gray-500 shadow-inner' // from user
-                : 'bg-blue-50 text-blue-500 dark:bg-white/10 dark:text-white/90', // from bot
-            )}>
-            {from === 'me' ? <User /> : <Bot />}
-          </div>
-        )}
         <div
           className={twMerge(
-            'rounded-lg',
+            'group relative rounded-xl',
             from === 'me'
-              ? 'bg-gray-200 text-gray-800' // from user
-              : 'bg-blue-50 text-slate-800 dark:bg-white/10 dark:text-white/90', // from bot
+              ? 'rounded-br-sm border border-gray-200 text-gray-800 dark:border-neutral-600' // from user
+              : 'rounded-bl-sm bg-gray-50 text-gray-800 dark:bg-neutral-600/50 dark:text-white/90', // from bot
           )}>
-          {from === 'ai' && (
-            <div className="flex items-center justify-between rounded-t-lg bg-blue-100 px-4 py-2 text-sm font-bold dark:bg-black/10">
+          {from === 'ai' && showCopy && (
+            <div className="flex items-center justify-between rounded-t-lg bg-gray-100 px-3 py-2 text-sm font-bold dark:bg-neutral-800/70">
               <div className="flex items-center gap-2">
                 <Bot size={16} />
                 AI 小助手
@@ -51,8 +40,7 @@ function Message({ from, content }: { from: 'me' | 'ai'; content: string }) {
               </button>
             </div>
           )}
-          <Markdown
-            className={twMerge('prose prose-sm break-all px-4 py-2 text-sm', from === 'me' ? '' : 'dark:prose-invert')}>
+          <Markdown className="prose prose-sm prose-neutral break-all px-3 py-2 text-sm dark:prose-invert">
             {content}
           </Markdown>
         </div>
@@ -130,36 +118,34 @@ export default function SpeechAI() {
     <>
       {active ? (
         <motion.div
-          className="fixed right-4 w-[min(400px,calc(100vw-32px))]"
+          className="fixed right-4 w-[min(400px,calc(100vw-32px))] rounded-lg bg-neutral-50 shadow-lg dark:bg-neutral-700"
           key={1}
           layoutId="speech-ai"
           style={{ bottom: y }}>
-          <motion.div
-            className="flex items-center justify-between rounded-t-lg bg-blue-500 p-2 text-white"
-            layoutId="speech-ai-header">
-            <div className="flex items-center gap-2 px-2 font-semibold">
-              <motion.div layoutId="speech-ai-header-icon">
-                <BotMessageSquare size={24} />
-              </motion.div>
+          <motion.div className="flex items-center justify-between gap-4 p-1" layoutId="speech-ai-header">
+            <div className="flex items-center font-semibold text-neutral-700 dark:text-neutral-500">
+              <motion.span className="p-2">
+                <BotMessageSquare size={20} />
+              </motion.span>
               AI 小助手
             </div>
-            <div className="flex items-center gap-2">
+            <div className="flex">
               <button
-                className="rounded-lg p-2 text-white hover:bg-black/5 active:bg-black/10"
+                className="rounded-full p-2 text-neutral-700 hover:bg-neutral-100 dark:text-neutral-500 dark:hover:bg-neutral-600"
                 onClick={() => setMessages([])}>
-                <RotateCcw size={24} />
+                <RotateCcw size={20} />
               </button>
               <button
-                className="rounded-lg p-2 text-white hover:bg-black/5 active:bg-black/10"
+                className="rounded-full p-2 text-neutral-700 hover:bg-neutral-100 dark:text-neutral-500 dark:hover:bg-neutral-600"
                 onClick={() => setActive(false)}>
-                <Minus size={24} />
+                <Minus size={20} />
               </button>
             </div>
           </motion.div>
           <motion.div
-            className="h-[400px] overflow-y-scroll bg-white/90 p-2 shadow-lg backdrop-blur-xl dark:bg-[#232323]/90"
+            className="h-[400px] overflow-y-scroll bg-white p-2 dark:bg-neutral-800"
             ref={messageContainerRef}>
-            <Message from="ai" content="嗨，我是這個網站的 AI 小助手！有什麼可以幫助你的？" />
+            <Message from="ai" content="嗨，我是這個網站的 AI 小助手！有什麼可以幫助你的？" showCopy={false} />
             <AnimatePresence>
               {messages.map((m, index) => (
                 <Message from={m.role === 'user' ? 'me' : 'ai'} content={m.content} key={index} />
@@ -168,62 +154,60 @@ export default function SpeechAI() {
             <AnimatePresence>
               {!isLoading && (
                 <motion.div
-                  className="flex flex-col gap-2 pl-12 text-blue-800 dark:text-pink-100"
+                  className="flex flex-col gap-2 text-gray-800 dark:text-gray-100"
                   initial={{ opacity: 0, y: 20 }}
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, y: 20 }}>
                   {[
                     `整理這頁的重點`,
                     `提供相關的背景資訊`,
-                    `這頁的主要觀點是什麼？`,
-                    '可以給我這個主題的詳細解釋嗎？',
+                    `這頁的主要觀點是什麼`,
+                    '可以給我這個主題的詳細解釋嗎',
                     '幫我生成一個這段內容的問答',
                   ]
                     .filter((x) => !messages.some((m) => m.content === x))
                     .map((message, index) => (
                       <button
                         onClick={() => sendDefaultMessage(message)}
-                        className="flex items-center gap-2 text-left text-sm hover:opacity-75 active:opacity-50"
+                        className="flex items-center gap-0.5 pl-3 text-left text-sm opacity-75 transition-all hover:gap-1 hover:opacity-100 active:opacity-50"
                         key={index}>
-                        <MessageCircleQuestion />
                         {message}
+                        <ArrowRight size={16} strokeWidth={1.5} />
                       </button>
                     ))}
                 </motion.div>
               )}
             </AnimatePresence>
           </motion.div>
-
           <form
-            className="rounded-b-lg bg-slate-100/90 p-2 text-blue-800 shadow-xl backdrop-blur-xl dark:bg-[#252525]/90 dark:text-blue-100"
+            className="flex items-center justify-between gap-1 overflow-hidden rounded-b-lg p-1"
             onSubmit={onSubmit}>
-            <div className="flex gap-2">
-              <input
-                className="w-full rounded-lg bg-white/90 p-2 outline-none dark:bg-white/10"
-                placeholder="在此輸入文字⋯⋯"
-                value={input}
-                onChange={handleInputChange}
-                required
-              />
-              <button
-                type="submit"
-                ref={submitButtonRef}
-                className="shrink-0 rounded-lg bg-blue-500 p-2 text-white hover:bg-blue-600 active:bg-blue-700">
-                <Send />
-              </button>
-            </div>
+            <input
+              className={twMerge(
+                'w-full flex-1 rounded-md rounded-bl-lg bg-transparent p-2 outline-none focus:border-transparent focus:shadow-none focus:ring-0',
+              )}
+              value={input}
+              onChange={handleInputChange}
+              required
+            />
+            <button
+              type="submit"
+              className="mr-1 rounded-full p-2 text-neutral-700 hover:bg-blue-100 dark:text-neutral-500 dark:hover:bg-neutral-600"
+              ref={submitButtonRef}>
+              <Send size={20} />
+            </button>
           </form>
         </motion.div>
       ) : (
         <motion.div className="fixed right-4" key={0} layoutId="speech-ai" style={{ bottom: y }}>
           <motion.button
-            className="rounded-lg bg-blue-500 p-4 text-white shadow-lg"
+            className="rounded-full bg-neutral-100 p-3 text-neutral-500 hover:shadow-md dark:bg-neutral-600 dark:text-neutral-50"
             onClick={() => setActive(true)}
             layoutId="speech-ai-header"
             whileTap={{ scale: 0.95 }}
-            whileHover={{ scale: 1.05 }}>
+            whileHover={{ scale: 1.05, y: -2 }}>
             <motion.div layoutId="speech-ai-header-icon">
-              <BotMessageSquare size={24} />
+              <BotMessageSquare size={20} />
             </motion.div>
           </motion.button>
         </motion.div>
