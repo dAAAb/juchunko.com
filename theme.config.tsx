b74aacb7e1ea38e9e5f5e62489e98b9f2b20a010
gnehs <gnehs@gnehs.net>
2024-06-12 12:37:51 +0800

    added dynamic og tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,3 +1,4 @@
+import { useConfig } from 'nextra-theme-docs'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { useRouter } from 'next/router'
 import Footer from './components/footer'
@@ -44,18 +45,24 @@ const config: DocsThemeConfig = {
       }
     }
   },
-  head: (
-    <>
-      <link rel="icon" type="image/svg+xml" href="/favicon.png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="先進國會・共識未來 - 科技立委葛如鈞．寶博士" />
-      <meta
-        property="og:description"
-        content="歡迎來到葛如鈞（寶博士）的寶博宇宙說明書，在這裡你可以學會如何操作、使用和召喚多元宇宙列車以及公僕寶博。"
-      />
-      <meta property="og:image" content="https://juchunko.com/ogimage.png" />
-    </>
-  ),
+  head: () => {
+    const { frontMatter } = useConfig()
+    return (
+      <>
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content={`${frontMatter.title} - 科技立委葛如鈞．寶博士` || '先進國會・共識未來 - 科技立委葛如鈞．寶博士'}
+        />
+        <meta
+          property="og:description"
+          content="歡迎來到葛如鈞（寶博士）的寶博宇宙說明書，在這裡你可以學會如何操作、使用和召喚多元宇宙列車以及公僕寶博。"
+        />
+        <meta property="og:image" content="https://juchunko.com/ogimage.png" />
+      </>
+    )
+  },
   main({ children }) {
     const pathname = usePathname()
     return (
