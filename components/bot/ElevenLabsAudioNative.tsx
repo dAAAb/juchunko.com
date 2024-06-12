83af73e29d6e8c39f099a0585af74bfcc5cd6deb
gnehs <gnehs@gnehs.net>
2024-06-12 13:25:03 +0800

    updated ElevenLabsAudioNative style

--- components/bot/ElevenLabsAudioNative.tsx
+++ components/bot/ElevenLabsAudioNative.tsx
@@ -29,19 +29,21 @@ export const ElevenLabsAudioNative = ({ publicUserId, children }: ElevenLabsProp
   }, [pathname])
 
   return isDocs ? (
-    <div
-      id="elevenlabs-audionative-widget"
-      data-height="90"
-      data-width="100%"
-      data-frameborder="no"
-      data-scrolling="no"
-      data-publicuserid={publicUserId}
-      data-playerurl="https://elevenlabs.io/player/index.html"
-      data-small="True"
-      data-textcolor={colorScheme === 'light' ? 'rgba(0, 0, 0, 1.0)' : 'rgba(255, 255, 255, 1.0)'}
-      data-backgroundcolor={colorScheme === 'light' ? 'rgba(255, 255, 255, 1.0)' : 'rgba(17, 17, 17, 1)'}
-      key={pathname}>
-      {children}
+    <div className="h-[90px] w-full overflow-hidden rounded-lg">
+      <div
+        id="elevenlabs-audionative-widget"
+        data-height="90"
+        data-width="100%"
+        data-frameborder="no"
+        data-scrolling="no"
+        data-publicuserid={publicUserId}
+        data-playerurl="https://elevenlabs.io/player/index.html"
+        data-small="True"
+        data-textcolor={colorScheme === 'light' ? 'rgba(0, 0, 0, 1.0)' : 'rgba(255, 255, 255, 1.0)'}
+        data-backgroundcolor={colorScheme === 'light' ? 'rgba(255, 255, 255, 1.0)' : 'rgba(17, 17, 17, 1)'}
+        key={pathname}>
+        {children}
+      </div>{' '}
     </div>
   ) : (
     <> </>
